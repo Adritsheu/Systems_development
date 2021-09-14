607ffbcb79d8525fa9b501f104dc51f331e6f1eb
Adritsheu <atrejosheu@g.harvard.edu>
2021-09-14 15:24:17 -0400

    Updated P3F.sh

--- homework/HW1/HW1-final/P3F.sh
+++ homework/HW1/HW1-final/P3F.sh
@@ -2,10 +2,9 @@
 # script for Question 3 part F
 # use a for loop
 
-
-for i in `ls`; do
-    echo "$i"
-    wc -l < $i
+for i in $(find . -maxdepth 1 -type f -name "*.*"); do
+    wordcount=$(cat "$i" |wc -l)
+    echo "$i $wordcount"
 done
  
 
