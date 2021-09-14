17435db20bbd20213506b161a5ce1fcf0898afa1
Adritsheu <atrejosheu@g.harvard.edu>
2021-09-14 15:24:05 -0400

    Updated P3.sh

--- homework/HW1/HW1-final/P3.sh
+++ homework/HW1/HW1-final/P3.sh
@@ -5,18 +5,18 @@
 
  grep '[0-9]*' apollo13.txt | wc -l   
 
-#Command to part C: **need help
-grep --help | grep '(\-count)(.*)'
+#Command to part C:
+grep --help | grep '^\s*\-c.*'
  
 
 #Command to part D:
 
  find *.py | wc -l
  
-#Command 1 for part E: **need help DO NOT have permission
+#Command 1 for part E:
  
  ls -l | find . -type f ! -perm -o=rw | wc -l
 
  
- #Command 2 for part E: **need help just add directories
+ #Command 2 for part E:
  ls -l | find . -maxdepth 1 -type df ! -perm -o=rw ! -name . | wc -l
